3906abd2d7b3f4c7faaff375c565f108deedbacb
Sergio Eduardo Aguilera Ramirez <26151257@aguascalientes.tecnm.mx>
2026-07-31 21:33:49 -0600

    Contar pares e impares

--- contar_par_impar.py
+++ contar_par_impar.py
@@ -2,6 +2,7 @@
 def contar_pares_impares():
     pares = 0
     impares = 0
+#Aumentar cantidad si los numeros van siendo pares o impares
     for num in numeros:
         if num % 2 == 0:
             pares += 1
@@ -10,10 +11,12 @@ def contar_pares_impares():
     return pares, impares
 
 numeros = []
+#Creada la lista ir añadiendo los 10 numeros enteros que el usuario ingrese
 for i in range(10):
     num = int(input("Numero {}: ".format(i + 1)))
     numeros.append(num)
-
+#Llamar la funcion e imprimir 
 p,i = contar_pares_impares()
 print("Pares.: ",p)
 print("Impares: ",i)
+#Hola Mundo #1
